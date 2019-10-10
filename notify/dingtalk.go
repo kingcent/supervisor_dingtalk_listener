81d30e5e2a0573c450a02b62707cb09c8962a23f
xm <xiaxianmao@thefifthera.com>
2019-10-11 00:25:33 +0800

    debug dingtalk push

--- notify/dingtalk.go
+++ notify/dingtalk.go
@@ -1,11 +1,13 @@
 package notify
 
 import (
+	"log"
 	"os"
 
 	"github.com/irebit/dingtalk_group_robot"
 )
 
 func DingTalk(title, desc string) {
+	log.Println("os.Args[1]", os.Args[1])
 	dingtalk_group_robot.New().SetAccessToken(os.Args[1]).Send(dingtalk_group_robot.NewLink().SetContent(title, desc, "", ""))
 }
