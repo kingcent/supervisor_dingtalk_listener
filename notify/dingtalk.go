bcde3c5503ad4c530d33c94a127e525954aa2279
xm <xiaxianmao@thefifthera.com>
2019-10-11 00:55:12 +0800

    debug

--- notify/dingtalk.go
+++ notify/dingtalk.go
@@ -7,7 +7,7 @@ import (
 	"github.com/irebit/dingtalk_group_robot"
 )
 
-func DingTalk(title, desc string) {
-	log.Println("os.Args[1]", os.Args[1])
-	dingtalk_group_robot.New().SetAccessToken(os.Args[1]).Send(dingtalk_group_robot.NewLink().SetContent(title, desc, "", ""))
+func DingTalk(title, text, messageUrl string) {
+	log.Println("os.Args[1]", os.Args[1], title, text, messageUrl)
+	dingtalk_group_robot.New().SetAccessToken(os.Args[1]).Send(dingtalk_group_robot.NewLink().SetContent(title, text, messageUrl, ""))
 }
